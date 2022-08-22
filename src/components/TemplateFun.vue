e0b99f4c3e31e437af07332899032be5d0d3172a
panda1 <blueblue0011@gmail.com>
2022-08-22 02:10:12 -0700

    data been modify to be objects inside array.  v-for to display all

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
         <section v-if = "show_post">
-            <h1>{{blog_title}}</h1><br>
-            <h3>{{blog_content}}</h3><br>
-            <img :src ="blog_image_url"><br>
+            <article v-for="(user, index) in content_users" :key="index">
+                <h1>{{content_users[index][`blog_title`]}}</h1><br>
+                <h3>{{content_users[index][`blog_content`]}}</h3><br>
+                <img :src = "content_users[index][`blog_image_url`]">
+                <!-- <h3>{{blog_content}}</h3><br> -->
+                <!-- <img :src ="blog_image_url"><br> -->
+            </article>
             
         </section>
         <button @click="change_button()">Change</button>
@@ -14,15 +18,29 @@
 
 
     export default {
-
           data() {
     return {
-      blog_content: `this is the quality blog content`,
-      blog_title: `Read this please`,
+        content_users:[{
+      blog_content: `this is the quality blog content 1`,
+      blog_title: `Read this please 1`,
       blog_image_url: `https://projectpokemon.org/images/shiny-sprite/gyarados-mega.gif`,
       show_post: true,
+    },
+    {
+      blog_content: `this is the quality blog content 2`,
+      blog_title: `Read this please 2`,
+      blog_image_url: `https://projectpokemon.org/images/shiny-sprite/magikarp-f.gif`,
+      show_post: true,
+    },
+    {
+      blog_content: `this is the quality blog content 3`,
+      blog_title: `Read this please 3`,
+      blog_image_url: `https://projectpokemon.org/images/shiny-sprite/omastar.gif`,
+      show_post: true,
+    }],
+    show_post: true,
     }
-  },        
+},        
   methods: {
             change_button() {
                 if(this.show_post){
