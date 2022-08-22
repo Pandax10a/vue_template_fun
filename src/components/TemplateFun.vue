95416422d60fc3395aa205b1a9cb965d9e96082a
panda1 <blueblue0011@gmail.com>
2022-08-22 01:34:33 -0700

    got the 2nd knowledge check to work with on off show content

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <h1>{{blog_title}}</h1><br>
-        <h3>{{blog_content}}</h3><br>
-        <img :src ="blog_image_url">
+        <section v-if = "show_post">
+            <h1>{{blog_title}}</h1><br>
+            <h3>{{blog_content}}</h3><br>
+            <img :src ="blog_image_url"><br>
+            
+        </section>
+        <button @click="change_button()">Change</button>
     </div>
 </template>
 
@@ -10,13 +14,24 @@
 
 
     export default {
+
           data() {
     return {
       blog_content: `this is the quality blog content`,
       blog_title: `Read this please`,
-      blog_image_url: `https://projectpokemon.org/images/shiny-sprite/gyarados-mega.gif`
+      blog_image_url: `https://projectpokemon.org/images/shiny-sprite/gyarados-mega.gif`,
+      show_post: true,
     }
-  },
+  },        
+  methods: {
+            change_button() {
+                if(this.show_post){
+                    this.show_post = false;
+                } else if (this.show_post === false) {
+                    this.show_post = true;
+                }
+            }
+        },
       name: 'template-fun'
     }
 </script>
